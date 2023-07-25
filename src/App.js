a0de6d0f56d23e6fabe36e20290b79e3f00de6fb
Rob Pickering <rob@pickering.org>
2023-07-25 23:51:58 +0100

    Routing fix for logged in case

--- src/App.js
+++ src/App.js
@@ -77,6 +77,7 @@ export default function App() {
             <Routes>
               <Route path="/" element={<Home {...messages} />} />
             </Routes>
+            <Navigate to="/" />
           </Router>
         }
         <ErrorAlert {...messages} />
